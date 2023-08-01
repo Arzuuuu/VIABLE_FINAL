89a70b964d4ed4d44b8e706b79a55a27ca4d12e9
Arzuuuu <arzu22786@gmail.com>
2023-08-02 01:25:47 +0530

    final_commit

--- forum_page.js
+++ forum_page.js
@@ -37,7 +37,22 @@ document.addEventListener('DOMContentLoaded', function () {
 // JavaScript to handle the toggle of the navigation links on smaller screens
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
+const hamburgerIcon = document.querySelector(".fa-bars");
 
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("show");
+  hamburgerIcon.classList.toggle("white-icon"); 
+});
+
+function isClickOutsideElement(event, element) {
+  return !element.contains(event.target);
+}
+
+document.addEventListener("click", (event) => {
+  if (isClickOutsideElement(event, navLinks) && isClickOutsideElement(event, menuToggle)) {
+    navLinks.classList.remove("show");
+    if (hamburgerIcon.classList.contains("white-icon")) {
+      hamburgerIcon.classList.remove("white-icon"); // Remove white color class when hiding the navLinks
+    }
+  }
 });
